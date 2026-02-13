bf43d60f9f542713b1484d5732e5c5c7a6c99274
Ruijie Su <suruijie0421@gmail.com>
2026-02-13 22:37:20 +0800

    enable github pages and fix errors

--- astro.config.ts
+++ astro.config.ts
@@ -19,15 +19,15 @@ import { remarkContainerDirectives } from './src/plugins/remark-container-direct
 import { remarkLeafDirectives } from './src/plugins/remark-leaf-directives.mjs'
 import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs'
 
-const { url: _site } = themeConfig.site
+const { url: site } = themeConfig.site
 const { imageHostURL } = themeConfig.preload ?? {}
 const imageConfig = imageHostURL
   ? { image: { domains: [imageHostURL], remotePatterns: [{ protocol: 'https' }] } }
   : {}
 
 export default defineConfig({
-  site: 'https://Eijiurus.github.io',
-  base: '/blog',
+  site,
+  base,
   trailingSlash: 'always', // Not recommended to change
   prefetch: {
     prefetchAll: true,
